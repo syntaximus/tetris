bb2b68ffc8c6232f470fe738fc758077ad0b4d50
Maciej Jakubiak <syntaximus@outlook.com>
2016-03-31 16:36:49 +0200

    Menu główne

---
+++ Tetris/Tetris/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Tetris.ViewModels
+{
+    public class MainWindowViewModel : PropertyChangedBase
+    {
+
+        private Visibility _recordsVisibility;
+        private Visibility _gameVisibility;
+        private Visibility _menuVisibility;
+
+        public Visibility MenuVisibility
+        {
+            get { return _menuVisibility; }
+
+            set
+            {
+                _menuVisibility = value;
+                NotifyOfPropertyChange("MenuVisibility");
+            }
+        }
+        public Visibility GameVisibility
+        {
+            get { return _menuVisibility; }
+
+            set
+            {
+                _menuVisibility = value;
+                NotifyOfPropertyChange("GameVisibility");
+            }
+        }
+        public Visibility RecordsVisibility
+        {
+            get { return _menuVisibility; }
+
+            set
+            {
+                _menuVisibility = value;
+                NotifyOfPropertyChange("RecordsVisibility");
+            }
+        }
+
+
+
+
+
+        public void Exit()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        public void NewGame()
+        {
+            MenuVisibility = Visibility.Hidden;
+
+        }
+        
+
+    }
+}
--- Tetris/Tetris/Views/MainWindowView.xaml.cs
+++ Tetris/Tetris/Views/MainWindowView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Tetris
+namespace Tetris.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindowView : Window
     {
-        public MainWindow()
+        public MainWindowView()
         {
             InitializeComponent();
         }
