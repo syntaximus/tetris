f5dc37ed39735ef7acdb28e4c8b198dbed2796a1
Maciej Jakubiak <syntaximus@outlook.com>
2016-04-09 20:03:17 +0200

    ViewModel - komentarze metod i własciwości.

--- Tetris/Tetris/ViewModels/MainWindowViewModel.cs
+++ Tetris/Tetris/ViewModels/MainWindowViewModel.cs
@@ -13,7 +13,7 @@ using Caliburn.Micro;
 using Tetris.Model;
 
 namespace Tetris.ViewModels
-{
+{ 
     [Serializable]
     public class Record
     {
@@ -52,6 +52,9 @@ namespace Tetris.ViewModels
         #endregion
 
         #region PROPERTIES
+        /// <summary>
+        /// Właściwość odpowiedzialna za widoczność menu głównego.
+        /// </summary>
         public Visibility MenuVisibility
         {
             get { return _menuVisibility; }
@@ -62,6 +65,9 @@ namespace Tetris.ViewModels
                 NotifyOfPropertyChange("MenuVisibility");
             }
         }
+        /// <summary>
+        /// Właściwość odpowiedzialna za widoczność ekranu końcowego z zapisaniem wyniku gry.
+        /// </summary>
         public Visibility EndGameVisibility
         {
             get { return _endGameVisibility; }
@@ -72,6 +78,9 @@ namespace Tetris.ViewModels
                 NotifyOfPropertyChange("EndGameVisibility");
             }
         }
+        /// <summary>
+        /// Właściwość odpowiedzialna za widoczność ekranu gry.
+        /// </summary>
         public Visibility GameVisibility
         {
             get { return _gameVisibility; }
@@ -82,6 +91,9 @@ namespace Tetris.ViewModels
                 NotifyOfPropertyChange("GameVisibility");
             }
         }
+        /// <summary>
+        /// Właściwość odpowiedzialna za widoczność tabeli rekordów.
+        /// </summary>
         public Visibility RecordsVisibility
         {
             get { return _recordsVisibility; }
@@ -92,6 +104,9 @@ namespace Tetris.ViewModels
                 NotifyOfPropertyChange("RecordsVisibility");
             }
         }
+        /// <summary>
+        /// Właściwość odpowiedzialna za widoczność poziomu podczas gry.
+        /// </summary>
         public string Level
         {
             get
@@ -104,6 +119,9 @@ namespace Tetris.ViewModels
                 NotifyOfPropertyChange("Level");
             }
         }
+        /// <summary>
+        /// Właściwość odpowiedzialna za widoczność ilości punktów podczas gry.
+        /// </summary>
         public string Points
         {
             get
@@ -116,6 +134,9 @@ namespace Tetris.ViewModels
                 NotifyOfPropertyChange("Points");
             }
         }
+        /// <summary>
+        /// Właściowość odpowiedzialna za wyświetlenie ilości punktów podczas zakończenia gry.
+        /// </summary>
         public string RecordPoints
         {
             get
@@ -128,9 +149,21 @@ namespace Tetris.ViewModels
                 NotifyOfPropertyChange("RecordPoints");
             }
         }
+        /// <summary>
+        /// Właściwość odpowiedzialna za przetrzymywanie nazwy gracza wpisanego w textboxie na konie rozgrywki.
+        /// </summary>
         public string PlayerName { get; set; }
+        /// <summary>
+        /// Kolekcja elementów, która jest wyświetlana na canvasie w czasie gry.
+        /// </summary>
         public ObservableCollection<RectItem> RectItems { get; set; }
+        /// <summary>
+        /// Kolekcja elementów, która jest wyświetlana w okienku "następny klocek".
+        /// </summary>
         public ObservableCollection<RectItem> NextBlockRectItems { get; set; }
+        /// <summary>
+        /// Kolejkca elementów z wynikami gry, które są wyświetlane w tablicy rekordów.
+        /// </summary>
         public BindableCollection<Record> RecordsList
         {
 
@@ -142,11 +175,17 @@ namespace Tetris.ViewModels
             }
 
         }
+        /// <summary>
+        /// Instancja gry, która jest odpowiedzialna za logikę rozgrywki.
+        /// </summary>
         public Game Game { get; set; }
 
         #endregion
 
         #region PUBLIC METHODS
+        /// <summary>
+        /// Kontruktor. Wyświetla menu główne i inicjalizuje zmienne. 
+        /// </summary>
         public MainWindowViewModel()
         {
             MenuVisibility = Visibility.Visible;
@@ -156,27 +195,26 @@ namespace Tetris.ViewModels
             RectItems = new ObservableCollection<RectItem>();
             NextBlockRectItems = new ObservableCollection<RectItem>();
         }
+        /// <summary>
+        /// Funkcja wykonuje się po kliknięciu w przycisk "Nowa gra". Rozpoczyna nową grę i uruchamia timery aktualizujące wyświetlanie ilości punktów i poziomu.
+        /// </summary>
         public void StartNewGame()
         {
-            try
-            {
-                Game = new Game(RectItems, NextBlockRectItems);
-                Game.GameCompleted += GameOnGameCompleted;
-                MenuVisibility = Visibility.Hidden;
-                RecordsVisibility = Visibility.Hidden;
-                GameVisibility = Visibility.Visible;
-                EndGameVisibility = Visibility.Hidden;
+            Game = new Game(RectItems, NextBlockRectItems);
+            Game.GameCompleted += GameOnGameCompleted;
+            MenuVisibility = Visibility.Hidden;
+            RecordsVisibility = Visibility.Hidden;
+            GameVisibility = Visibility.Visible;
+            EndGameVisibility = Visibility.Hidden;
 
-                var notifyOfPropertyChangeDispatcherTimer = new DispatcherTimer();
-                notifyOfPropertyChangeDispatcherTimer.Tick += NotifyOfPropertyChange_Tick;
-                notifyOfPropertyChangeDispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, ControlsVisibleInterval);
-                notifyOfPropertyChangeDispatcherTimer.Start();
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
+            var notifyOfPropertyChangeDispatcherTimer = new DispatcherTimer();
+            notifyOfPropertyChangeDispatcherTimer.Tick += NotifyOfPropertyChange_Tick;
+            notifyOfPropertyChangeDispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, ControlsVisibleInterval);
+            notifyOfPropertyChangeDispatcherTimer.Start();
         }
+        /// <summary>
+        /// Funkcja wykonuje się po kliknięciu w przycisk "Najalepsze wyniki". Wczytuje z pliku 10 najlepszych wyników i aktualizuj kolejkcje służącą za wyświetlanie wyników.
+        /// </summary>
         public void ShowRecords()
         {
             MenuVisibility = Visibility.Hidden;
@@ -203,13 +241,19 @@ namespace Tetris.ViewModels
                 recrd.Add(records[i]);
             RecordsList = recrd;
         }
+        /// <summary>
+        /// Funkcja wykonuje się po kliknięciu w przycisk "Wyjście". Zamyka aplikacje.
+        /// </summary>
         public void Exit()
         {
             Application.Current.MainWindow.Close();
         }
+        /// <summary>
+        /// Funkcja wykonuje się po kliknięciu w przycisk "Zapisz wynik" po zakończeniu gry (o ile wynik gracza jest w najlepszych 10 wynikach). Zapisuje ten wynik do pliku.
+        /// </summary>
         public void SaveRecord()
         {
-            Record record = new Record(Int32.Parse(Points), ProcessPlayerName(PlayerName));
+            Record record = new Record(Game.Points, ProcessPlayerName(PlayerName));
             List<Record> records;
             IFormatter formatter = new BinaryFormatter();
             using (
@@ -245,6 +289,9 @@ namespace Tetris.ViewModels
             EndGameVisibility = Visibility.Hidden;
 
         }
+        /// <summary>
+        /// Funkcja wykonuje się po kliknięciu w przycisk "Ok" w czasie wyśweitlania najlepszych wyników. Wyświetla wtedy menu.
+        /// </summary>
         public void RecordsOk()
         {
             MenuVisibility = Visibility.Visible;
@@ -252,6 +299,10 @@ namespace Tetris.ViewModels
             GameVisibility = Visibility.Hidden;
             EndGameVisibility = Visibility.Hidden;
         }
+        /// <summary>
+        /// Funkcja przechwytuje zdarzenia kliknięcia w klawiature przez użytkownika, a następnie przekazuje je do gry (o ile aktualnie prowadzone jest rozgrywka).
+        /// </summary>
+        /// <param name="context"></param>
         public void ExecuteKeyboardCommand(ActionExecutionContext context)
         {
             var keyArgs = context.EventArgs as KeyEventArgs;
@@ -264,6 +315,11 @@ namespace Tetris.ViewModels
         #endregion
 
         #region PRIVATE METHODS
+        /// <summary>
+        /// Przetwarze nazwę gracza (zapobiega wpisaniu pustej nazwy i nazwy o długości powyżej 16).
+        /// </summary>
+        /// <param name="playerName"></param>
+        /// <returns></returns>
         private string ProcessPlayerName(string playerName)
         {
             if (playerName == null)
@@ -273,6 +329,11 @@ namespace Tetris.ViewModels
             return playerName;
 
         }
+        /// <summary>
+        /// Obsłużenie zdarzenia końca gry. Jeżeli gra się skończyła i gracz zdobył ilość punktów znajdującą się w 10 najlepszych wynikach, to wyświetla się ekran zapisu wyniku. W przeciwnym wypadku wyświetla się menu. Gra zostaje zakończona.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
         private void GameOnGameCompleted(object sender, EventArgs eventArgs)
         {
             {
@@ -292,7 +353,7 @@ namespace Tetris.ViewModels
                     }
                 }
                 records = records.OrderByDescending(x => x.Points).ToList();
-                if (records.Count < 10 || records[records.Count - 1].Points < Int32.Parse(Points))
+                if (records.Count < 10 || records[records.Count - 1].Points < Game.Points)
                 {
                     NotifyOfPropertyChange("RecordPoints");
                     MenuVisibility = Visibility.Hidden;
@@ -312,21 +373,15 @@ namespace Tetris.ViewModels
 
             };
         }
+        /// <summary>
+        /// Tick timera odpowiedzialny za aktualizacje punktów i poziomu w View.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void NotifyOfPropertyChange_Tick(object sender, EventArgs e)
         {
-            try
-            {
                 NotifyOfPropertyChange("Points");
                 NotifyOfPropertyChange("Level");
-
-            }
-            catch (Exception ew)
-            {
-
-
-                throw ew;
-            }
-
         }
         #endregion
     }
