d3a448f66995859bf963defc267de0ce01aa26d9
Maciej Jakubiak <syntaximus@outlook.com>
2016-05-14 02:07:10 +0200

    e

--- Tetris/Tetris/Model/Blocks/Class1.cs
+++ Tetris/Tetris/Model/Blocks/Class1.cs
@@ -1,59 +1,43 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows.Media;
+﻿using System.Windows.Media;
 
-//namespace Tetris.Model.Blocks
-//{
-//    internal class BlockAnia : Block
-//    {
-//        public BlockAnia()
-//        {
-//            /*
-//            1 1 0 0
-//            0 1 1 0
-//            0 0 0 0
-//            0 0 0 0
+namespace Tetris.Model.Blocks
+{
+    internal class BlockOmega : Block
+    {
+        public BlockOmega()
+        {
+            Surface = new[,]
+            {
+                {true, false, false, true},
+                {false, true, true, false},
+                {false, true, true, false},
+                {true, false, false, true}
+            };
+            Color = new SolidColorBrush(Colors.Orange);
+        }
 
-//            0 1 0 0
-//            1 1 0 0
-//            1 0 0 0
-//            0 0 0 0
-//            */
-//            Surface = new[,]
-//            {
-//                {true, false, false, true},
-//                {false, true, true, false},
-//                {false, true, true, false},
-//                {true, false, false, true}
-//            };
-//            Color = new SolidColorBrush(Colors.Pink);
-//        }
-
-//        public override bool TryRotate()
-//        {
-//            if (CanRotate)
-//            {
-//                Surface = ShowRotate();
-//                return true;
-//            }
-//            return false;
-//        }
+        public override bool TryRotate()
+        {
+            if (CanRotate)
+            {
+                Surface = ShowRotate();
+                return true;
+            }
+            return false;
+        }
 
 
-//        public override bool[,] ShowRotate()
-//        {
-//            var rotateSurface = new bool[4, 4];
-//            for (var i = 0; i < 4; i++)
-//            {
-//                for (var j = 0; j < 4; j++)
-//                {
-//                    rotateSurface[j, i] = Surface[3 - i, j];
-//                }
-//            }
-//            return rotateSurface;
-//        }
-//    }
-//}
+        public override bool[,] ShowRotate()
+        {
+            var rotateSurface = new bool[4, 4];
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 4; j++)
+                {
+                    rotateSurface[j, i] = Surface[3 - i, j];
+                }
+            }
+            return rotateSurface;
+        }
+    }
+}
